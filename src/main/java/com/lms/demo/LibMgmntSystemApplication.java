2b2710ef7fef88330a2b9f62c1d97db40647eac7
guhan0000 <guhanm0000@gmail.com>
2025-12-22 13:25:58 +0530

    db properties dependencies added

---
+++ src/main/java/com/lms/demo/LibMgmntSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMgmntSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibMgmntSystemApplication.class, args);
+	}
+
+}
