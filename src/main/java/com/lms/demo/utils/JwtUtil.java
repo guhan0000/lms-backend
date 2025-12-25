9d621c5860b1f70b5b6d3a25d311c8c402dea851
guhan0000 <guhanm0000@gmail.com>
2025-12-25 19:29:53 +0530

    new commit on jwt

--- src/main/java/com/lms/demo/utils/JwtUtil.java
+++ src/main/java/com/lms/demo/utils/JwtUtil.java
@@ -1,4 +1,65 @@
 package com.lms.demo.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class JwtUtil {
+    private  final String SECRET="Guhan is a good boy since 2002";
+    private final long  EXPIRTION=1000*60*60;
+    private final Key SECRET_KEY= Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String email, String role){
+        Map<String,Object>claims=new HashMap<>();
+        claims.put("role",role);
+        return createToken(claims,email);
+    }
+    public String createToken(Map<String, Object> claims,String subject){
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRTION))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String extractUserName(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    public String extractRole(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role").toString();
+    }
+    public Date extractExpiration(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+    public Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    public Boolean validateToken(String token,String email){
+         final String extractedEmail=extractUserName(token);
+         return (extractedEmail.equals(email) && !isTokenExpired(token));
+    }
+
 }
